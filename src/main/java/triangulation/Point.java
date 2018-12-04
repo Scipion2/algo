eb1b5c524d195522e07d1d5ee9cd741f2a53d4b6
Gaming <a>
2018-12-04 23:45:42 +0100

    a

--- src/main/java/triangulation/Point.java
+++ src/main/java/triangulation/Point.java
@@ -23,4 +23,14 @@ public class Point {
         return this.pos_y;
 
     }
+
+    public void drawPoint(graphicsContext)
+    {
+
+        graphicsContext.setFill(new Color(0,0,1,0.5));
+        graphicsContext.fillOval(graphicsContext.setFill(new Color(0,0,1,0.5));
+        graphicsContext.fillOval(this.pos_x,this.pos_y,10,10);
+
+    }
+
 }
