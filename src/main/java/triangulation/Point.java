f45c20ccab9254b65780be23a90e084d3909a8e3
Gaming <a>
2018-12-05 10:38:32 +0100

    a

--- src/main/java/triangulation/Point.java
+++ src/main/java/triangulation/Point.java
@@ -1,5 +1,8 @@
 package triangulation;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class Point {
     int pos_x;
     int pos_y;
@@ -24,11 +27,10 @@ public class Point {
 
     }
 
-    public void drawPoint(graphicsContext)
+    public void drawPoint(GraphicsContext graphicsContext)
     {
 
         graphicsContext.setFill(new Color(0,0,1,0.5));
-        graphicsContext.fillOval(graphicsContext.setFill(new Color(0,0,1,0.5));
         graphicsContext.fillOval(this.pos_x,this.pos_y,10,10);
 
     }
