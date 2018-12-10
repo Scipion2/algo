3f86a1fc837b7a823cd76bef629a30c1b79852a8
Anthony <f>
2018-12-10 22:52:39 +0100

    convexe done

--- src/main/java/triangulation/Convexe.java
+++ src/main/java/triangulation/Convexe.java
@@ -1,6 +1,8 @@
 package triangulation;
 
 
+import utils.QuickSort;
+
 import java.awt.*;
 import java.util.*;
 import java.util.List;
@@ -16,6 +18,7 @@ public class Convexe
     public Convexe(List<Point> src)
     {
 
+        System.out.println("test");
         this.convPolygon=get_convex(src);
         for(int i=0;i<convPolygon.size();++i)
         {
@@ -24,107 +27,115 @@ public class Convexe
 
         }
 
+        System.out.println("test2");
+
+
     }
 
-    public static double surface(Point firstPoint, Point seconPoint, Point thirdPoint)
+    private static void partition(List<Point> cloud, List<Point> convPolygon)
     {
-        // retourne la surface
-        // négatif si on tourne dans le sens contraire des aiguilles d'une montre
-        // positif sinon
-        return ((seconPoint.getX() - firstPoint.getX()) * (thirdPoint.getY() - firstPoint.getY()) - (thirdPoint.getX() - firstPoint.getX()) * (seconPoint.getY() - firstPoint.getY())) / 2.;
-    }
-
-    private static void partition(List<Point> cloud, List<Point> conv)
-    {
-        conv.clear();
-        if(cloud.size()<=3)
+        convPolygon.clear();
+        if(cloud.size()<=3) // cas d'arrêt
         {
-            conv.addAll(cloud);
-            if(cloud.size()==3 && surface(conv.get(0), conv.get(1), conv.get(2))>0)
+            convPolygon.addAll(cloud);
+            if(cloud.size()==3 && isGoingUpDown(convPolygon.get(0), convPolygon.get(1), convPolygon.get(2))>0)
             {
-                Point point = conv.get(1);
-                conv.remove(point); conv.add(point);
+                Point point = convPolygon.get(1);
+                convPolygon.remove(point); convPolygon.add(point);
             }
             return;
         }
-        //On divise en deux
-        List<Point> firstHalf = new ArrayList<Point>();
-        List<Point> secondHalf = new ArrayList<Point>();
+
+
+        // Sépare le nuage de point en deux parties
+        List<Point> leftPart = new ArrayList<Point>();
+        List<Point> rightPart = new ArrayList<Point>();
 
         for(int i = 0; i<cloud.size()/2; ++i)
-            firstHalf.add(cloud.get(i));
+            leftPart.add(cloud.get(i));
 
         for(int i = cloud.size()/2; i<cloud.size(); ++i)
-            secondHalf.add(cloud.get(i));
+            rightPart.add(cloud.get(i));
 
-        // On calcule les enveloppes de chaque morceau
-        List<Point> convFirstHalf = new ArrayList<Point>();
-        List<Point> convSecondHalf = new ArrayList<Point>();
-        partition(firstHalf, convFirstHalf);
-        partition(secondHalf, convSecondHalf);
+        // Création des enveloppes convexes des deux parties
+        List<Point> convLeftPart = new ArrayList<Point>();
+        List<Point> convRightPart = new ArrayList<Point>();
+        partition(leftPart, convLeftPart);
+        partition(rightPart, convRightPart);
 
-        // et on fusionne !
-        conv.addAll( fusion(convFirstHalf, convSecondHalf) );
+        // Fusion des deux parties
+        convPolygon.addAll( fusion(convLeftPart, convRightPart) );
     }
 
-    private static List<Point> fusion(List<Point> firstHalf, List<Point> secondHalf)
+    private static List<Point> fusion(List<Point> leftPart, List<Point> rightPart)
     {
-        // recherche du point d'abcisse le plus grand dans firstHalf;
+        // recherche du point le plus a droite dans la partie gauche;
         int iMax = 0;
-        for( int i = 1; i < firstHalf.size(); ++i) if(firstHalf.get(i).getX() > firstHalf.get(iMax).getX()) iMax = i;
-        // le point d'abcisse le plus petit dans secondHalf est en 0
+        for( int i = 1; i < leftPart.size(); ++i) if(leftPart.get(i).getX() > leftPart.get(iMax).getX()) iMax = i;
+        // le point le plus a gauche de la partie droite est forcément 0
         int iMin = 0;
-        // le segment du haut
-        int ip1 = iMax;
-        int ip2 = iMin;
+
+        // Recherche des points de fusions en haut
+        int actualUpLeftPoint = iMax;
+        int actualUpRightPoint = iMin;
         boolean verif = true;
         while(verif){
             verif = false;
-            if(surface(secondHalf.get(ip2), firstHalf.get(ip1), firstHalf.get((ip1+1)%firstHalf.size()))>0){
-                ip1 = (ip1+1)%firstHalf.size();
+            if(isGoingUpDown(rightPart.get(actualUpRightPoint), leftPart.get(actualUpLeftPoint), leftPart.get((actualUpLeftPoint+1)%leftPart.size()))>0){
+                actualUpLeftPoint = (actualUpLeftPoint+1)%leftPart.size();
                 verif = true;
             }
-            if(surface(firstHalf.get(ip1), secondHalf.get(ip2), secondHalf.get((ip2-1+secondHalf.size())%secondHalf.size()))<0){
-                ip2 = (ip2-1+secondHalf.size())%secondHalf.size();
+            if(isGoingUpDown(leftPart.get(actualUpLeftPoint), rightPart.get(actualUpRightPoint), rightPart.get((actualUpRightPoint-1+rightPart.size())%rightPart.size()))<0){
+                actualUpRightPoint = (actualUpRightPoint-1+rightPart.size())%rightPart.size();
                 verif = true;
             }
         }
-        // le segment du bas
-        int im1 = iMax;
-        int im2 = iMin;
+
+
+        // recherche des points de fusion en bas
+        int actualDownLeftPoint = iMax;
+        int actualDownRightPoint = iMin;
         verif = true;
         while(verif){
             verif = false;
-            if(surface(secondHalf.get(im2), firstHalf.get(im1), firstHalf.get((im1-1+firstHalf.size())%firstHalf.size()))<0){
-                im1 = (im1-1+firstHalf.size())%firstHalf.size();
+            if(isGoingUpDown(rightPart.get(actualDownRightPoint), leftPart.get(actualDownLeftPoint), leftPart.get((actualDownLeftPoint-1+leftPart.size())%leftPart.size()))<0){
+                actualDownLeftPoint = (actualDownLeftPoint-1+leftPart.size())%leftPart.size();
                 verif = true;
             }
-            if(surface(firstHalf.get(im1), secondHalf.get(im2), secondHalf.get((im2+1)%secondHalf.size()))>0){
-                im2 = (im2+1)%secondHalf.size();
+            if(isGoingUpDown(leftPart.get(actualDownLeftPoint), rightPart.get(actualDownRightPoint), rightPart.get((actualDownRightPoint+1)%rightPart.size()))>0){
+                actualDownRightPoint = (actualDownRightPoint+1)%rightPart.size();
                 verif = true;
             }
         }
-        // fabrication de l'enveloppe
-        List<Point> env = new ArrayList();
-        for( int i = 0; i<=im1; ++i)            env.add(firstHalf.get(i));
-        if(ip2==0){
-            for( int i = im2; i<secondHalf.size(); ++i) env.add(secondHalf.get(i));
-            env.add(secondHalf.get(0));
+
+
+        // Création de l'enveloppe convexe
+        ArrayList<Point> enveloppeConvexe = new ArrayList();
+        for( int i = 0; i<=actualDownLeftPoint; ++i)            enveloppeConvexe.add(leftPart.get(i));
+        if(actualUpRightPoint==0){
+            for( int i = actualDownRightPoint; i<rightPart.size(); ++i) enveloppeConvexe.add(rightPart.get(i));
+            enveloppeConvexe.add(rightPart.get(0));
         }
         else
-            for( int i = im2;  i<=ip2; ++i)            env.add(secondHalf.get(i));
-        if(ip1!=0) for( int i = ip1; i<firstHalf.size(); ++i) env.add(firstHalf.get(i));
+            for( int i = actualDownRightPoint;  i<=actualUpRightPoint; ++i)            enveloppeConvexe.add(rightPart.get(i));
+        if(actualUpLeftPoint!=0) for( int i = actualUpLeftPoint; i<leftPart.size(); ++i) enveloppeConvexe.add(leftPart.get(i));
 
-        return env;
+
+        for(int i =0; i<enveloppeConvexe.size();i++)
+            System.out.println("x= " +enveloppeConvexe.get(i).getX() +", y= " +enveloppeConvexe.get(i).getY());
+        System.out.println("");
+        System.out.println();
+
+        return enveloppeConvexe;
     }
 
-    //on prend un arraylist de points en entrée et on renvoie un array list qui représente l'enveloppe convexe.
+
     private static List<Point> get_convex(List<Point> cloud)
     {
-        List<Point> env=new ArrayList<>();
-        cloud.sort(comparingInt(Point::getX));
-        partition(cloud, env);
-        return env;
+        List<Point> enveloppeConvexe=new ArrayList<>();
+        QuickSort.quickSort(enveloppeConvexe, 0, enveloppeConvexe.size());
+        partition(cloud, enveloppeConvexe);
+        return enveloppeConvexe;
     }
 
     static void isInConvexe(List<Point> convexe, List<Point> all, List<Point> not_conv){
@@ -137,8 +148,16 @@ public class Convexe
     }
 
 
-    void drawConvexe(Graphics graphics, int factorx, int factory)
+    public static double isGoingUpDown(Point A, Point B, Point C) // >0 si BC plus haut que BA, sinon plus bas.
     {
+        return ((B.getX() - A.getX()) * (C.getY() - A.getY()) - (C.getX() - A.getX()) * (B.getY() - A.getY())) / 2.;
+    }
+
+
+    public void drawConvexe(Graphics graphics,int factorx,int factory)
+    {
+
+        System.out.println("draw");
 
         for(int i=0;i<convPolygon.size()-1;++i)
         {
